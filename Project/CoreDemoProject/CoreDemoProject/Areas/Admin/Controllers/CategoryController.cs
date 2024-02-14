4243af73209fc2f4d0ce861b94f57a4991f9cfab
CavidAtamoghlanov <catamoglanov@gmail.com>
2024-02-14 02:22:41 -0800

    93 kimi

--- Project/CoreDemoProject/CoreDemoProject/Areas/Admin/Controllers/CategoryController.cs
+++ Project/CoreDemoProject/CoreDemoProject/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Conctrets;
+using DataAccessLayer.MyEntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace CoreDemoProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index(int page=1)
         {
-            return View();
+            var values = categoryManager.GetList().ToPagedList(page, 3);
+            return View(values);
         }
     }
 }
